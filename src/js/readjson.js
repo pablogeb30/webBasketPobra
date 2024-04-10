6cdbbb7b7258b9bdcf27f3c50936025ad4fc60f4
pablogeb30 <pagebas@me.com>
2024-04-10 18:04:09 +0200

    pendejo

--- src/js/readjson.js
+++ src/js/readjson.js
@@ -1,34 +1,35 @@
 // Esta función se ejecutará automáticamente cuando el script se cargue
-function loadAndDisplayData() {
+function loadAndDisplayData(jsonURL, elementID) {
   var xhttp = new XMLHttpRequest();
 
-  xhttp.open("GET", "/assets/json/equipos.json", true);
+  xhttp.open("GET", jsonURL, true);
   xhttp.send();
 
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const equipo = JSON.parse(this.responseText);
-      let text = "<h2>" + equipo.equipo + "</h2>";
-      text += "<table border='0'>";
+      let text = "<h3>" + equipo.equipo + "</h3>";
+      text += "<table border='0' cellspacing='20'>";
       text +=
-        "<tr><th>Nombre</th><th>Edad</th><th>Altura(cm)</th><th>Posición</th></tr>";
+        "<tr><th style='text-align: center;'>Nombre</th><th>Edad</th><th>Altura(cm)</th><th>Posición</th></tr>";
       for (let i = 0; i < equipo.jugadores.length; i++) {
         text +=
-          "<tr><td>" +
+          "<tr><td style='text-align: center;'>" +
           equipo.jugadores[i].nombre +
-          "</td><td>" +
+          "</td><td style='text-align: center;'>" +
           equipo.jugadores[i].edad +
-          "</td><td>" +
+          "</td><td style='text-align: center;'>" +
           equipo.jugadores[i].altura +
-          "</td><td>" +
+          "</td><td style='text-align: center;'>" +
           equipo.jugadores[i].posicion +
           "</td></tr>";
       }
       text += "</table>";
-      document.getElementById("jugadores").innerHTML = text;
+      document.getElementById(elementID).innerHTML = text;
     }
   };
 }
 
-// Llama a la función para cargar y mostrar los datos cuando el script se carga
-loadAndDisplayData();
+// Llama a la función dos veces para cargar y mostrar los datos (senior masculino y femenino)
+loadAndDisplayData("/assets/json/seniorm.json", "seniorm");
+loadAndDisplayData("/assets/json/seniorf.json", "seniorf");
